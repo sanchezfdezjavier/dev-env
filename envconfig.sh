732b37cbb5207fd95cc5d63b1a051c86915ebe67
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-10-12 17:04:23 +0100

    Fix: Alias was created only in the current session, now it's persistent

--- envconfig.sh
+++ envconfig.sh
@@ -2,4 +2,4 @@ sudo apt update &&
 sudo apt install bat && 
 sudo apt install zsh &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-alias cat="batcat"
+echo "alias cat='batcat'" >> .zshrc
