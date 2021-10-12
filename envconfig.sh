f374984dd22a522c9e27635e8c70676819603211
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-10-12 14:29:51 +0100

    First commit

---
+++ envconfig.sh
@@ -0,0 +1,5 @@
+sudo apt update && 
+sudo apt install bat && 
+sudo apt install zsh &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+alias cat="batcat"
