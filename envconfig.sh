32cb6fd9f7d6c82ced6d29ad08d0378cd7977809
Ubuntu <ubuntu@ip-172-31-47-79.eu-west-1.compute.internal>
2021-10-20 09:30:05 +0000

    Feat: Comments in envconfig

--- envconfig.sh
+++ envconfig.sh
@@ -3,11 +3,16 @@
 echo "🟠 Installing..."
 
 sudo apt update -y && 
+
+# Install batcat for colored cat outputs --> https://github.com/sharkdp/bat
 sudo apt install bat -y && 
-sudo apt install zsh -y &&
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "alias cat='batcat'" >> .zshrc
 
+# Install zsh, and interactive shell better than bash --> https://github.com/ohmyzsh/ohmyzsh
+sudo apt install zsh -y &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Prettier git log command
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 echo "✅ Installation successful"
