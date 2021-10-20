18bb562fb730a58a7ac668a65ecc40125a8deb04
Ubuntu <ubuntu@ip-172-31-47-79.eu-west-1.compute.internal>
2021-10-20 09:24:25 +0000

    Feat: git lg alias added to envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
+echo "🟠 Installing..."
+
 sudo apt update -y && 
 sudo apt install bat -y && 
 sudo apt install zsh -y &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "alias cat='batcat'" >> .zshrc
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 echo "✅ Installation successful"
