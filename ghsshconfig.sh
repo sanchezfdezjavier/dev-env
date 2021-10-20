65b5b4764e774269f45787d17c908acde3d4fa9e
Ubuntu <ubuntu@ip-172-31-47-79.eu-west-1.compute.internal>
2021-10-20 08:41:37 +0000

    Feat: Message now prettier

--- ghsshconfig.sh
+++ ghsshconfig.sh
@@ -2,6 +2,8 @@ ssh-keygen -t ed25519 -C "sanchezfdez.javier@gmail.com" &&
 eval "$(ssh-agent -s)" &&
 ssh-add ~/.ssh/id_ed25519 &&
 cat ~/.ssh/id_ed25519.pub &&
+echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
 echo "COPY THE KEY AND PASTE IT INTO GITHUB"
+echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
 
 
