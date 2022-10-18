b9f7a218b428a9b9cc7217edf060d12cdb602473
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-10-18 09:03:53 +0000

    Feat: Adds git config script

---
+++ git-config.sh
@@ -0,0 +1,6 @@
+echo "Configuring git"
+git config --global user.name "Javier Sanchez"
+git config --global user.email "sanchezfdez.javier@gmail.com"
+git config --global core.editor vi
+echo "Git successfully configured\n\n"
+git config --list
