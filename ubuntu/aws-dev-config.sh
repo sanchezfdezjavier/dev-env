ca2daab3c13ad972a69dccbfade5e8b69776b5f1
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-11-03 10:14:32 +0000

    Feat: adds aws dev config script

---
+++ ubuntu/aws-dev-config.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# Updates apt
+sudo apt update -qq
+
+# Improve git default log command
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# Sets git config
+git clone https://github.com/sanchezfdezjavier/dev-env.git
+cd dev-env
+chmod +x git-config.sh
+./git-config.sh
+
+# Installs virtualenv
+sudo apt install python3-virtualenv -y
+sudo apt install python3-pip
+sudo pip3 install virtualenv
+
+# Creates virtualenv
+virtualenv venv
+source venv/bin/activate
+
+# Installs aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Installs eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# Installs charmcraft
+sudo snap install charmcraft --classic
+
+# Installs juju
+sudo snap install juju --classic
+
+# Generates todos
+echo "##############################################"
+echo "Configure aws cli"
+echo "##############################################"
+echo "##############################################"
+echo "Configure Github SSH key"
+echo "##############################################"
+echo "✅ Done! ⚠️ But don't forget to configure all of the above."
